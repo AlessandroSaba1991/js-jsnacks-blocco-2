3697ad5590898e80eb0ed41e05e5e6b4bcff5d5c
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-03-31 14:30:37 +0200

    add snack 3

---
+++ js-snack-3/main.js
@@ -0,0 +1,16 @@
+/* 
+Snack3
+Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+*/
+
+const nomi = ['Alessandro', 'Giulio', 'Claudio', 'Franco', 'Pino'];
+const cognomi = ['Saba', 'Rovella', 'De Palma', 'Ruggiero', 'Giovannini'];
+const invitati = []
+
+for (let i = 0; invitati.length < nomi.length; i++) {
+    const nome = nomi[i];
+    const cognome = cognomi[i];
+    invitati.push(`${nome} ${cognome}`)
+}
+
+console.log(invitati);
