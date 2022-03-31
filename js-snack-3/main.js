2e77b4f4b2aa57626696bebf2b8510a9dcc7c014
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-03-31 16:41:04 +0200

    snack 3 creata lista super random senza ripetizioni sia di nome che di cognome

--- js-snack-3/main.js
+++ js-snack-3/main.js
@@ -7,20 +7,62 @@ const nomi = ['Alessandro', 'Giulio', 'Claudio', 'Franco', 'Pino'];
 const cognomi = ['Saba', 'Rovella', 'De Palma', 'Ruggiero', 'Giovannini'];
 const invitati = []
 
-for (let i = 0; invitati.length < nomi.length; i++) {
+/* for (let i = 0; invitati.length < nomi.length; i++) {
     const nome = nomi[i];
     const cognome = cognomi[i];
     invitati.push(`${nome} ${cognome}`)
 }
 
-console.log(invitati);
+console.log(invitati); */
 
-let c = 0;
+/* let c = 0;
 
 while (invitati.length != nomi.length) {
     const nome = nomi[c];
     const cognome = cognomi[c];
     invitati.push(`${nome} ${cognome}`)
     c++
+}
+console.log(invitati); */
+
+//lista random che più random non si può
+
+const numeri_random_nomi = [];
+const numeri_random_cognomi = []
+
+
+while (invitati.length != nomi.length) {
+
+    let nome, cognome;
+
+    //genero numero random per i normi
+    let num_random = Math.floor(Math.random() * nomi.length);
+
+    //controllo se il numero generato è stato già generato
+    while (numeri_random_nomi.includes(num_random)) {
+
+        num_random = Math.floor(Math.random() * nomi.length);
+
+    }
+
+    //se non è incluso lo aggiungo alla lista dei numeri generati
+    numeri_random_nomi.push(num_random);
+
+    //Genero il nome random
+    nome = nomi[num_random];
+
+    //Ripeto per i cognomi
+    num_random = Math.floor(Math.random() * cognomi.length);
+    while (numeri_random_cognomi.includes(num_random)) {
+
+        num_random = Math.floor(Math.random() * cognomi.length);
+
+    }
+    numeri_random_cognomi.push(num_random);
+    cognome = cognomi[num_random];
+
+
+    //aggiungo alla lista invitati
+    invitati.push(`${nome} ${cognome}`)
 }
 console.log(invitati);
